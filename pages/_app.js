e988abe079fbdcb488dac3bd7264419945e7f75c
unknown <aramirez.gagroup@gmail.com>
2026-08-31 12:24:50 -0500

    Cambios Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 import '../styles/global.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }){
+  const showSpeed = process.env.NODE_ENV === 'production';
   return (
     <SessionProvider session={session}>
+      {showSpeed && <SpeedInsights />}
       <Component {...pageProps} />
     </SessionProvider>
   )
